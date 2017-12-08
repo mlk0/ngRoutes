a78cae71cdeae54b34a6bfb2a88722cc17e7748d
Mirko Atanasov <mirko.atanasov@dh.com>
2017-12-08 02:35:29 -0500

    added parameterized route samples. the optinal parameters is implemented through the routerLink arguments instead of definition of multiple routes https://codecraft.tv/courses/angular/routing/parameterised-routes

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BaseComponent } from '../base.component';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-products',
@@ -7,12 +9,27 @@ import { BaseComponent } from '../base.component';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent extends BaseComponent implements OnInit {
+  productId: any;
 
-  constructor() {
+  constructor(activatedRoute: ActivatedRoute) {
     super();
+
+    activatedRoute.params.subscribe(routeParameters => {
+      if (routeParameters) {
+        let id = routeParameters["id"];
+        console.log(`routeParameters["id"] = ${id}`);
+        this.productId = id;
+      }
+      else {
+        console.log('no route params passed');
+      }
+    }
+    );
+
   }
 
   ngOnInit() {
+    super.ngOnInit();
   }
 
 }
